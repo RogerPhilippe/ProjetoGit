bd4e178d8627542c79cf29e8e66863a73c5c00e4
rogerphilippepereira <rogerphilippe@outlook.com>
2025-05-21 21:55:20 -0300

    Changed print message

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello Git");
+        JOptionPane.showMessageDialog(null, "Hello Git, from develop");
     }
 }
